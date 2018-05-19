b37780878225b0035a3a1de1a3d39f5788124994
Tahir Naushasd <t.naushad25@gmail.com>
2018-05-19 13:59:50 +0100

    azure ad b2c example

---
+++ Fiver.Security.AzureAdB2C/Controllers/SecurityController.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+
+namespace Fiver.Security.AzureAdB2C.Controllers
+{
+    public class SecurityController : Controller
+    {
+        public IActionResult Login()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "B2C_1_sign_in");
+        }
+
+        [HttpPost]
+        public async Task Logout()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var scheme = User.FindFirst("tfp").Value;
+            await HttpContext.SignOutAsync(scheme);
+        }
+
+        [Route("signup")]
+        public async Task<IActionResult> SignUp()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "B2C_1_sign_up");
+        }
+
+        [Route("editprofile")]
+        public IActionResult EditProfile()
+        {
+            return Challenge(new AuthenticationProperties { RedirectUri = "/" }, "B2C_1_edit_profile");
+        }
+    }
+}
--- Fiver.Security.AzureAdB2C/Startup.cs
+++ Fiver.Security.AzureAdB2C/Startup.cs
@@ -1,34 +1,77 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 namespace Fiver.Security.AzureAdB2C
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        private readonly string TenantName = ""; // aka 'Initial Domain Name' in Azure
+        private readonly string ClientId = ""; // aka 'Application Id' in Azure
+
         public void ConfigureServices(IServiceCollection services)
         {
+            var signUpPolicy = "B2C_1_sign_up";
+            var signInPolicy = "B2C_1_sign_in";
+            var signUpInPolicy = "B2C_1_sign_up_in";
+            var editProfilePolicy = "B2C_1_edit_profile";
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = signUpInPolicy;
+            })
+            .AddOpenIdConnect(signUpPolicy, GetOpenIdConnectOptions(signUpPolicy))
+            .AddOpenIdConnect(signInPolicy, GetOpenIdConnectOptions(signInPolicy))
+            .AddOpenIdConnect(signUpInPolicy, GetOpenIdConnectOptions(signUpInPolicy))
+            .AddOpenIdConnect(editProfilePolicy, GetOpenIdConnectOptions(editProfilePolicy))
+            .AddCookie();
+
+            services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
+            app.UseDeveloperExceptionPage();
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseAuthentication();
+
+            app.UseMvcWithDefaultRoute();
         }
+
+        #region " Private "
+
+        private Action<OpenIdConnectOptions> GetOpenIdConnectOptions(string policy)
+            => options =>
+            {
+                options.MetadataAddress = "https://login.microsoftonline.com/" + this.TenantName + "/v2.0/.well-known/openid-configuration?p=" + policy;
+                options.ClientId = this.ClientId;
+                options.ResponseType = OpenIdConnectResponseType.IdToken;
+                options.CallbackPath = "/signin/" + policy;
+                options.SignedOutCallbackPath = "/signout/" + policy;
+                options.SignedOutRedirectUri = "/";
+                options.TokenValidationParameters.NameClaimType = "name";
+
+                options.Events.OnMessageReceived = context =>
+                {
+                    if (!string.IsNullOrEmpty(context.ProtocolMessage.Error) &&
+                        !string.IsNullOrEmpty(context.ProtocolMessage.ErrorDescription) &&
+                        context.ProtocolMessage.ErrorDescription.StartsWith("AADB2C90091"))
+                    {
+                        context.Response.Redirect("/");
+                        context.HandleResponse();
+                    }
+
+                    return Task.FromResult(0);
+                };
+            };
+
+        #endregion
     }
 }
